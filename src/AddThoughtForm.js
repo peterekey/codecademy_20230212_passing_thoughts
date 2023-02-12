a1a198789305d16f692bcd62de6e4b59bc113c72
Peter Key <petekey@duck.com>
2023-02-13 09:07:38 +1100

    Completed up to step 5

--- src/AddThoughtForm.js
+++ src/AddThoughtForm.js
@@ -1,15 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { generateId, getNewExpirationTime } from './utilities';
 
 export function AddThoughtForm(props) {
+  const [text, setText] = useState('');
+
+  const handleTextChange = ({target}) => {
+    setText(target.value);
+  }
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    
+  }
+  
   return (
-    <form className="AddThoughtForm">
+    <form 
+      className="AddThoughtForm" 
+      onSubmit={handleSubmit} 
+    >
       <input
         type="text"
         aria-label="What's on your mind?"
         placeholder="What's on your mind?"
+        value={text}
+        onChange={handleTextChange}
       />
-      <input type="submit" value="Add" />
+      <input 
+        type="submit" 
+        value="Add"
+      />
     </form>
   );
 }
--- src/App.js
+++ src/App.js
@@ -18,13 +18,17 @@ function App() {
     },
   ]);
 
+  const addThought = (thought) => {
+    setThoughts((prev) => [thought, ...prev]);
+  }
+
   return (
     <div className="App">
       <header>
         <h1>Passing Thoughts</h1>
       </header>
       <main>
-        <AddThoughtForm />
+        <AddThoughtForm addThought={addThought}/>
         <ul className="thoughts">
           {thoughts.map((thought) => (
             <Thought key={thought.id} thought={thought} />
